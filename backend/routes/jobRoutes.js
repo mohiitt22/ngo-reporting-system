8fe73f1810550b6a65a117761a5ba756ba863b0b
mohiitt22 <131586633+mohiitt22@users.noreply.github.com>
2025-12-14 16:06:38 +0530

    Delete backend directory

--- backend/routes/jobRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getJobStatus } = require("../controllers/jobController");
-
-router.get("/:job_id", getJobStatus);
-
-module.exports = router;

754a68e41b95d3583ce02967ed12ccaa328c2fbb
mohiitt22 <131586633+mohiitt22@users.noreply.github.com>
2025-12-14 22:59:52 +0530

    Delete backend directory

--- backend/routes/dashboardRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getDashboard } = require("../controllers/dashboardController");
-
-router.get("/", getDashboard);
-
-module.exports = router;
--- backend/routes/reportRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { submitReport } = require("../controllers/reportController");
-
-router.post("/", submitReport);
-
-module.exports = router;
